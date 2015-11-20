e092a1083e962665041b750ae1f2a8143360f3b1
Carl Daver <cdaver@pivotal.io>
2015-11-20 02:13:42 -0500

    Initial Commits 	modified:   manifest.yml 	modified:   ui-search/pom.xml 	modified:   ui-search/src/main/java/service/Application.java 	modified:   ui-search/src/main/resources/application.yml 	deleted:    ui-search/src/main/resources/bootstrap.yml 	modified:   ui-search/target/classes/application.yml 	modified:   ui-search/target/classes/service/Application.class 	modified:   ui-search/target/classes/service/ui/MovieUI$2.class 	modified:   ui-search/target/classes/service/ui/UserUI$2.class 	modified:   ui-search/target/classes/service/ui/UserUI.class 	modified:   ui-search/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	modified:   ui-search/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst 	modified:   ui-search/target/surefire-reports/TEST-service.DemoApplicationTests.xml 	modified:   ui-search/target/surefire-reports/service.DemoApplicationTests.txt 	modified:   ui-search/target/ui-search-0.1.0.jar 	modified:   ui-search/target/ui-search-0.1.0.jar.original 	modified:   ui-search/ui-search.iml 	.gitignore

--- ui-search/src/main/java/service/Application.java
+++ ui-search/src/main/java/service/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -26,9 +28,10 @@ import java.io.IOException;
 @EnableFeignClients(basePackages = "service.clients")
 @EnableWebMvc
 @EnableAutoConfiguration
-@EnableZuulProxy
 @EnableVaadin
 @Controller
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
